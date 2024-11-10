b82aa0f3dfc59c3b8d1144b223e7f06c3230f67b
Roberto Luiz Debarba <roberto.debarba@gmail.com>
2024-11-09 23:37:28 -0300

    Testes de ProdutoWebController

--- src/main/java/com/goodfood/product/ProductApplication.java
+++ src/main/java/com/goodfood/product/ProductApplication.java
@@ -3,10 +3,8 @@ package com.goodfood.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.goodfood.product.gateways.database")
 @EnableCaching
 public class ProductApplication {
 
---
+++ src/test/java/com/goodfood/product/gateways/http/ProdutoWebControllerTest.java
@@ -0,0 +1,124 @@
+package com.goodfood.product.gateways.http;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.goodfood.product.domain.Produto;
+import com.goodfood.product.usecase.CriarProduto;
+import com.goodfood.product.usecase.EditarProduto;
+import com.goodfood.product.usecase.ObterProduto;
+import com.goodfood.product.usecase.ObterProdutoCategoria;
+
+@WebMvcTest(ProdutoWebController.class)
+class ProdutoWebControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ObterProduto obterProduto;
+
+    @MockBean
+    private CriarProduto criarProduto;
+
+    @MockBean
+    private EditarProduto editarProduto;
+
+    @MockBean
+    private ObterProdutoCategoria obterProdutoCategoria;
+
+    @Test
+    void cadastrarProdutoWithValidRequest() throws Exception {
+        Produto produto = Produto.builder()
+                .id(UUID.randomUUID())
+                .descricao("Coca-cola")
+                .preco(new BigDecimal("10.00"))
+                .categoria("BEBIDA")
+                .build();
+
+        when(criarProduto.execute(any())).thenReturn(produto);
+
+        mockMvc.perform(post("/produto")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{ \"descricao\": \"Coca-cola\", \"preco\": 10.00, \"categoria\": \"BEBIDA\" }"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/" + produto.getId()))
+                .andExpect(jsonPath("$.descricao").value("Coca-cola"))
+                .andExpect(jsonPath("$.preco").value(10.00))
+                .andExpect(jsonPath("$.categoria").value("BEBIDA"));
+    }
+
+    @Test
+    void editarProdutoWithValidRequest() throws Exception {
+        Produto produto = Produto.builder()
+                .id(UUID.randomUUID())
+                .descricao("Coca-cola")
+                .preco(new BigDecimal("10.00"))
+                .categoria("BEBIDA")
+                .build();
+
+        doNothing().when(editarProduto).execute(any(), any());
+
+        mockMvc.perform(put("/produto/" + produto.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{ \"descricao\": \"Coca-cola\", \"preco\": 10.00, \"categoria\": \"BEBIDA\" }"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void obterProdutoWithValidId() throws Exception {
+        Produto produto = Produto.builder()
+                .id(UUID.randomUUID())
+                .descricao("Coca-cola")
+                .preco(new BigDecimal("10.00"))
+                .categoria("BEBIDA")
+                .build();
+
+        when(obterProduto.execute(anyString())).thenReturn(produto);
+
+        mockMvc.perform(get("/produto")
+                                .param("id", produto.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.descricao").value("Coca-cola"))
+                .andExpect(jsonPath("$.preco").value(10.00))
+                .andExpect(jsonPath("$.categoria").value("BEBIDA"));
+    }
+
+    @Test
+    void buscarPorCategoriaWithValidCategory() throws Exception {
+        Produto produto = Produto.builder()
+                .id(UUID.randomUUID())
+                .descricao("Coca-cola")
+                .preco(new BigDecimal("10.00"))
+                .categoria("BEBIDA")
+                .build();
+
+        when(obterProdutoCategoria.execute(anyString())).thenReturn(Collections.singletonList(produto));
+
+        mockMvc.perform(get("/produto/categoria")
+                                .param("categoria", "BEBIDA"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].descricao").value("Coca-cola"))
+                .andExpect(jsonPath("$[0].preco").value(10.00))
+                .andExpect(jsonPath("$[0].categoria").value("BEBIDA"));
+    }
+}
